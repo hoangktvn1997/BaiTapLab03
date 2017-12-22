687fab5cc7465b9d4528d08d20970f1d79290182
Hoàng <hoangktvn1997@yahoo.com>
2017-12-22 10:15:54 +0700

    Completed Result Button

--- TinhUocBoiChung/Forms/frmTinhUocBoiChung.Designer.cs
+++ TinhUocBoiChung/Forms/frmTinhUocBoiChung.Designer.cs
@@ -29,19 +29,19 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumberB = new System.Windows.Forms.TextBox();
             this.txtNumberA = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtNumberB = new System.Windows.Forms.TextBox();
-            this.rdUCLN = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.rdBCNN = new System.Windows.Forms.RadioButton();
-            this.txtResult = new System.Windows.Forms.TextBox();
-            this.btnCheck = new System.Windows.Forms.Button();
-            this.btnCancel = new System.Windows.Forms.Button();
+            this.rdUCLN = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.btnExit = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -49,18 +49,67 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.AutoSize = true;
             this.groupBox1.Controls.Add(this.txtNumberB);
             this.groupBox1.Controls.Add(this.txtNumberA);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(18, 18);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(215, 129);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(320, 137);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Nhập số:";
+            // 
+            // txtNumberB
+            // 
+            this.txtNumberB.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNumberB.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumberB.Location = new System.Drawing.Point(104, 81);
+            this.txtNumberB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNumberB.Name = "txtNumberB";
+            this.txtNumberB.Size = new System.Drawing.Size(208, 26);
+            this.txtNumberB.TabIndex = 1;
+            // 
+            // txtNumberA
+            // 
+            this.txtNumberA.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNumberA.Cursor = System.Windows.Forms.Cursors.SizeWE;
+            this.txtNumberA.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumberA.Location = new System.Drawing.Point(104, 47);
+            this.txtNumberA.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNumberA.Name = "txtNumberA";
+            this.txtNumberA.Size = new System.Drawing.Size(208, 26);
+            this.txtNumberA.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 85);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nhập B:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 51);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập A:";
             // 
             // groupBox2
             // 
@@ -69,82 +118,21 @@
             this.groupBox2.Controls.Add(this.rdBCNN);
             this.groupBox2.Controls.Add(this.rdUCLN);
             this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(229, 12);
+            this.groupBox2.Location = new System.Drawing.Point(354, 18);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(186, 129);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(214, 137);
             this.groupBox2.TabIndex = 0;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Chọn phép tính:";
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.AutoSize = true;
-            this.groupBox3.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.groupBox3.Controls.Add(this.btnExit);
-            this.groupBox3.Controls.Add(this.btnCancel);
-            this.groupBox3.Controls.Add(this.btnCheck);
-            this.groupBox3.Controls.Add(this.txtResult);
-            this.groupBox3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox3.Location = new System.Drawing.Point(12, 147);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(403, 140);
-            this.groupBox3.TabIndex = 0;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Kết quả:";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(6, 35);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 19);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nhập A:";
-            // 
-            // txtNumberA
-            // 
-            this.txtNumberA.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNumberA.Location = new System.Drawing.Point(69, 32);
-            this.txtNumberA.Name = "txtNumberA";
-            this.txtNumberA.Size = new System.Drawing.Size(140, 26);
-            this.txtNumberA.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(6, 78);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 19);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Nhập B:";
-            // 
-            // txtNumberB
-            // 
-            this.txtNumberB.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNumberB.Location = new System.Drawing.Point(69, 75);
-            this.txtNumberB.Name = "txtNumberB";
-            this.txtNumberB.Size = new System.Drawing.Size(140, 26);
-            this.txtNumberB.TabIndex = 1;
-            // 
-            // rdUCLN
-            // 
-            this.rdUCLN.AutoSize = true;
-            this.rdUCLN.Font = new System.Drawing.Font("Times New Roman", 12F);
-            this.rdUCLN.Location = new System.Drawing.Point(6, 33);
-            this.rdUCLN.Name = "rdUCLN";
-            this.rdUCLN.Size = new System.Drawing.Size(174, 23);
-            this.rdUCLN.TabIndex = 0;
-            this.rdUCLN.TabStop = true;
-            this.rdUCLN.Text = "Tính Ước chung lớn nhất";
-            this.rdUCLN.UseVisualStyleBackColor = true;
             // 
             // rdBCNN
             // 
             this.rdBCNN.AutoSize = true;
             this.rdBCNN.Font = new System.Drawing.Font("Times New Roman", 12F);
-            this.rdBCNN.Location = new System.Drawing.Point(6, 76);
+            this.rdBCNN.Location = new System.Drawing.Point(8, 81);
+            this.rdBCNN.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdBCNN.Name = "rdBCNN";
             this.rdBCNN.Size = new System.Drawing.Size(172, 23);
             this.rdBCNN.TabIndex = 0;
@@ -152,54 +140,103 @@
             this.rdBCNN.Text = "Tính Bội chung nhỏ nhất";
             this.rdBCNN.UseVisualStyleBackColor = true;
             // 
-            // txtResult
+            // rdUCLN
             // 
-            this.txtResult.Enabled = false;
-            this.txtResult.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtResult.Location = new System.Drawing.Point(130, 28);
-            this.txtResult.Name = "txtResult";
-            this.txtResult.Size = new System.Drawing.Size(171, 29);
-            this.txtResult.TabIndex = 0;
-            this.txtResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.rdUCLN.AutoSize = true;
+            this.rdUCLN.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.rdUCLN.Location = new System.Drawing.Point(8, 47);
+            this.rdUCLN.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdUCLN.Name = "rdUCLN";
+            this.rdUCLN.Size = new System.Drawing.Size(174, 23);
+            this.rdUCLN.TabIndex = 0;
+            this.rdUCLN.TabStop = true;
+            this.rdUCLN.Text = "Tính Ước chung lớn nhất";
+            this.rdUCLN.UseVisualStyleBackColor = true;
             // 
-            // btnCheck
+            // groupBox3
             // 
-            this.btnCheck.Font = new System.Drawing.Font("Times New Roman", 12F);
-            this.btnCheck.Location = new System.Drawing.Point(60, 84);
-            this.btnCheck.Name = "btnCheck";
-            this.btnCheck.Size = new System.Drawing.Size(75, 28);
-            this.btnCheck.TabIndex = 1;
-            this.btnCheck.Text = "Tính";
-            this.btnCheck.UseVisualStyleBackColor = true;
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.AutoSize = true;
+            this.groupBox3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.groupBox3.Controls.Add(this.btnExit);
+            this.groupBox3.Controls.Add(this.btnCancel);
+            this.groupBox3.Controls.Add(this.btnResult);
+            this.groupBox3.Controls.Add(this.txtResult);
+            this.groupBox3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(18, 163);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(550, 214);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Kết quả:";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.btnExit.Location = new System.Drawing.Point(406, 126);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(112, 41);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // btnCancel
             // 
+            this.btnCancel.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btnCancel.Font = new System.Drawing.Font("Times New Roman", 12F);
-            this.btnCancel.Location = new System.Drawing.Point(180, 84);
+            this.btnCancel.Location = new System.Drawing.Point(223, 126);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 28);
+            this.btnCancel.Size = new System.Drawing.Size(112, 41);
             this.btnCancel.TabIndex = 1;
             this.btnCancel.Text = "Hủy bỏ";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
-            // btnExit
+            // btnResult
             // 
-            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 12F);
-            this.btnExit.Location = new System.Drawing.Point(299, 84);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(75, 28);
-            this.btnExit.TabIndex = 1;
-            this.btnExit.Text = "Thoát";
-            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnResult.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnResult.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.btnResult.Location = new System.Drawing.Point(30, 126);
+            this.btnResult.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(112, 41);
+            this.btnResult.TabIndex = 1;
+            this.btnResult.Text = "Tính";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Enabled = false;
+            this.txtResult.Font = new System.Drawing.Font("Times New Roman", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtResult.Location = new System.Drawing.Point(148, 51);
+            this.txtResult.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(254, 29);
+            this.txtResult.TabIndex = 0;
+            this.txtResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(427, 298);
+            this.ClientSize = new System.Drawing.Size(581, 383);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Tính ước chung lớn nhất - bội chung nhỏ nhất";
             this.groupBox1.ResumeLayout(false);
@@ -227,7 +264,7 @@
         private System.Windows.Forms.RadioButton rdUCLN;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Button btnCancel;
-        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnResult;
         private System.Windows.Forms.TextBox txtResult;
     }
 }
--- TinhUocBoiChung/Forms/frmTinhUocBoiChung.cs
+++ TinhUocBoiChung/Forms/frmTinhUocBoiChung.cs
@@ -12,9 +12,105 @@ namespace TinhUocBoiChung
 {
     public partial class Form1 : Form
     {
+        private int NumberA;
+        private int NumberB;
+
+        //Thông báo lỗi
+        string errorMessage;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //Lệnh Tính
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+            //Lấy giá trị A và B
+            string strA = txtNumberA.Text;
+            string strB = txtNumberB.Text;
+
+            //Kiểm tra A và B đã nhập đúng chưa ? Chưa thì hiện bảng thông báo lỗi
+            if (IsValidNumber(strA, strB) == true)
+            {
+                //Kiểm tra nếu chưa hoặc đã lựa chọn phép tính gì
+                if (!rdUCLN.Checked && !rdBCNN.Checked)
+                {
+                    MessageBox.Show("Vui long chon phep tinh.");
+                    return;
+                }
+
+                int Result;
+                if (rdUCLN.Checked)
+                    Result = TimUCLN();
+                else
+                    Result = TimBCNN();
+
+                //Hiển thị kết quả
+                txtResult.Text = Result.ToString();
+            }
+            else
+                MessageBox.Show(errorMessage);
+        }
+
+        //Hàm tính Ước chung lớn nhất & Bội chung nhỏ nhất
+        private int TimUCLN()
+        {
+            while (NumberA != NumberB)
+            {
+                if (NumberA > NumberB)
+                    NumberA -= NumberB;
+                else
+                    NumberB -= NumberA;
+            }
+            return NumberA;
+        }
+
+        private int TimBCNN()
+        {
+            return NumberA * NumberB / TimUCLN();
+        }
+
+        /// <summary>
+        /// Hàm kiểm tra 2 số nhập vào
+        /// return true: Nếu 2 số hợp lệ
+        /// return false: Nếu có ít nhất 1 số không hợp lệ
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        private  bool IsValidNumber(string a, string b)
+        {
+            errorMessage = "";
+
+            //Lỗi không nhập giá trị
+            if (string.IsNullOrEmpty(a))
+            {
+                errorMessage += "Vui long nhap so A!\n";
+            }
+
+            if (string.IsNullOrEmpty(b))
+            {
+                errorMessage += "Vui long nhap so B!\n";
+            }
+
+            //Lỗi nhập giá trị không hợp lệ
+            try
+            {
+                NumberA = int.Parse(a);
+                NumberB = int.Parse(b);
+            }
+            catch (Exception)
+            {
+                errorMessage += "Gia tri da nhap khong hop le!\n";
+            }
+
+            return (errorMessage == "") ? true : false;
+        }
+
+        //Lệnh Thoát
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+        //    Form1.Visible(false);
         }
     }
 }
